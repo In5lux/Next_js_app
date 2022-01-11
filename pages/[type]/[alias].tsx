cdf65ae9a6259aef849631fedba9b2d7823e7b38
In5lux <nikos_almeryda@mail.ru>
2022-01-12 00:41:39 +0300

    Sent review form data

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -11,6 +11,7 @@ import { ParsedUrlQuery } from 'node:querystring';
 import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
 import { TopPageComponent } from '../../page-components';
+import { API } from '../../helpers/api';
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
   return (
@@ -59,7 +60,7 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({
   }
   try {
     const { data: menu } = await axios.post<MenuItem[]>(
-      process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+      API.topPage.find,
       {
         firstCategory: firstCategoryItem.id,
       }
@@ -70,10 +71,10 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({
       };
     }
     const { data: page } = await axios.get<TopPageModel>(
-      process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/byAlias/' + params.alias
+      API.topPage.byAlias + params.alias
     );
     const { data: products } = await axios.post<ProductModel[]>(
-      process.env.NEXT_PUBLIC_DOMAIN + '/api/product/find',
+      API.product.find,
       {
         category: page.category,
         limit: 10,
