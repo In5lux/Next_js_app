da612e47e76eeda5e13d0174b646ae4a576b112d
In5lux <nikos_almeryda@mail.ru>
2022-01-18 20:08:16 +0300

    Sort keyboard accessibility

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -4,31 +4,31 @@ import SortIcon from './sort.svg';
 import cn from 'classnames';
 
 export const Sort = ({
-  sort,
-  setSort,
-  className,
-  ...props
+	sort,
+	setSort,
+	className,
+	...props
 }: SortProps): JSX.Element => {
-  return (
-    <div className={cn(styles.sort, className)} {...props}>
-      <span
-        onClick={() => setSort(SortEnum.Rating)}
-        className={cn({
-          [styles.active]: sort == SortEnum.Rating,
-        })}
-      >
-        <SortIcon className={styles.sortIcon} />
-        По рейтингу
-      </span>
-      <span
-        onClick={() => setSort(SortEnum.Price)}
-        className={cn({
-          [styles.active]: sort == SortEnum.Price,
-        })}
-      >
-        <SortIcon className={styles.sortIcon} />
-        По цене
-      </span>
-    </div>
-  );
+	return (
+		<div className={cn(styles.sort, className)} {...props}>
+			<button
+				onClick={() => setSort(SortEnum.Rating)}
+				className={cn({
+					[styles.active]: sort == SortEnum.Rating,
+				})}
+			>
+				<SortIcon className={styles.sortIcon} />
+				По рейтингу
+			</button>
+			<button
+				onClick={() => setSort(SortEnum.Price)}
+				className={cn({
+					[styles.active]: sort == SortEnum.Price,
+				})}
+			>
+				<SortIcon className={styles.sortIcon} />
+				По цене
+			</button>
+		</div>
+	);
 };
