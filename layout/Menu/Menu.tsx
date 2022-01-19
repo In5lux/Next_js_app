828f2afcd4478deec12386cd2cda3920d3f951ca
In5lux <nikos_almeryda@mail.ru>
2022-01-19 20:53:46 +0300

    Landmarks

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -133,5 +133,5 @@ export const Menu = (): JSX.Element => {
     ));
   };
 
-  return <div className={styles.menu}>{buildFirstLevel()}</div>;
+  return <nav className={styles.menu} role="navigation">{buildFirstLevel()}</nav>;
 };
