247a343420b296c008d5bcb3c7163cf0b27abfb6
In5lux <nikos_almeryda@mail.ru>
2022-01-19 10:20:51 +0300

    Aria-label и aria-labelledby

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -8,41 +8,42 @@ import GlassIcon from './glass.svg';
 import { useRouter } from 'next/router';
 
 export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
-  const [search, setSearch] = useState<string>('');
+	const [search, setSearch] = useState<string>('');
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const goToSearch = () => {
-    router.push({
-      pathname: '/search',
-      query: {
-        q: search,
-      },
-    });
-  };
+	const goToSearch = () => {
+		router.push({
+			pathname: '/search',
+			query: {
+				q: search,
+			},
+		});
+	};
 
-  const handleKeyDown = (e: KeyboardEvent) => {
-    if (e.key === 'Enter') {
-      goToSearch();
-    }
-  };
+	const handleKeyDown = (e: KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			goToSearch();
+		}
+	};
 
-  return (
-    <div className={cn(className, styles.search)} {...props}>
-      <Input
-        className={styles.input}
-        placeholder="Поиск..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      <Button
-        appearance="primary"
-        className={styles.buttonSearch}
-        onClick={goToSearch}
-      >
-        <GlassIcon />
-      </Button>
-    </div>
-  );
+	return (
+		<div className={cn(className, styles.search)} {...props}>
+			<Input
+				className={styles.input}
+				placeholder="Поиск..."
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+				onKeyDown={handleKeyDown}
+			/>
+			<Button
+				appearance="primary"
+				className={styles.buttonSearch}
+				onClick={goToSearch}
+				aria-label="Искать по сайту"
+			>
+				<GlassIcon />
+			</Button>
+		</div>
+	);
 };
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -6,73 +6,73 @@ import { TopLevelCategory } from '../../interfaces/page.interface';
 import { SortEnum } from '../../components/Sort/Sort.props';
 import { useEffect, useReducer } from 'react';
 import { sortReducer } from './sort.reducer';
-import { useScrollY } from '../../hooks/useScrollY';
+import { declOfNum } from '../../helpers/helpers';
 
 export const TopPageComponent = ({
-  page,
-  firstCategory,
-  products,
+	page,
+	firstCategory,
+	products,
 }: TopPageComponentProps): JSX.Element => {
-  const [{ products: sortedProducts, sort }, dispathSort] = useReducer(
-    sortReducer,
-    {
-      products,
-      sort: SortEnum.Rating,
-    }
-  );
-  const setSort = (sort: SortEnum) => {
-    dispathSort({ type: sort });
-  };
+	const [{ products: sortedProducts, sort }, dispathSort] = useReducer(
+		sortReducer,
+		{
+			products,
+			sort: SortEnum.Rating,
+		}
+	);
+	const setSort = (sort: SortEnum) => {
+		dispathSort({ type: sort });
+	};
 
-  useEffect(()=>{
-	dispathSort({type:'reset', initialState: products}); 
-  },[products]);
+	useEffect(() => {
+		dispathSort({ type: 'reset', initialState: products });
+	}, [products]);
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.title}>
-        {page && <Htag tag="h1">{page.title}</Htag>}
-        {products && (
-          <Tag color="grey" size="medium">
-            {products.length}
-          </Tag>
-        )}
-        <Sort sort={sort} setSort={setSort} />
-      </div>
-      <div>
-        {sortedProducts &&
-          sortedProducts.map((p) => <Product layout key={p._id} product={p} />)}
-      </div>
-      <div className={styles.hhTitle}>
-        {page && <Htag tag="h2">Вакансии - {page.category}</Htag>}
-        {products && (
-          <Tag color="red" size="medium">
-            hh.ru
-          </Tag>
-        )}
-      </div>
-      {firstCategory == TopLevelCategory.Courses && page.hh && (
-        <HhData {...page.hh} />
-      )}
-      {page && page.advantages && page.advantages.length > 0 && (
-        <>
-          <Htag tag="h2">Преимущества</Htag>
-          <Advantages advantages={page.advantages} />
-        </>
-      )}
-      {page && page.seoText && (
-        <div
-          className={styles.seo}
-          dangerouslySetInnerHTML={{ __html: page.seoText }}
-        />
-      )}
-      <Htag tag="h2">Получаемые навыки</Htag>
-      {page &&
-        page.tags.map((t) => (
-          <Tag color="primary" key={t}>
-            {t}
-          </Tag>
-        ))}
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.title}>
+				{page && <Htag tag="h1">{page.title}</Htag>}
+				{products && (
+					<Tag color="grey" size="medium" aria-label={products.length + declOfNum(products.length, ['элемент', 'элемента', 'элементов'])}>
+						{products.length}
+					</Tag>
+				)}
+				<Sort sort={sort} setSort={setSort} />
+			</div>
+			<div>
+				{sortedProducts &&
+					sortedProducts.map((p) => <Product layout key={p._id} product={p} />)}
+			</div>
+			<div className={styles.hhTitle}>
+				{page && <Htag tag="h2">Вакансии - {page.category}</Htag>}
+				{products && (
+					<Tag color="red" size="medium">
+						hh.ru
+					</Tag>
+				)}
+			</div>
+			{firstCategory == TopLevelCategory.Courses && page.hh && (
+				<HhData {...page.hh} />
+			)}
+			{page && page.advantages && page.advantages.length > 0 && (
+				<>
+					<Htag tag="h2">Преимущества</Htag>
+					<Advantages advantages={page.advantages} />
+				</>
+			)}
+			{page && page.seoText && (
+				<div
+					className={styles.seo}
+					dangerouslySetInnerHTML={{ __html: page.seoText }}
+				/>
+			)}
+			<Htag tag="h2">Получаемые навыки</Htag>
+			{page &&
+				page.tags.map((t) => (
+					<Tag color="primary" key={t}>
+						{t}
+					</Tag>
+				))}
+		</div>
+	);
 };
