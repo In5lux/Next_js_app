77efd078fe5d23ad51f84f21dbd8073d72c3d0ea
In5lux <nikos_almeryda@mail.ru>
2022-01-21 18:27:21 +0300

    Docker

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -10,24 +10,24 @@ export const Up = (): JSX.Element => {
 	const controls = useAnimation();
 	const y = useScrollY();
 
-	useEffect(()=>{
-		controls.start({opacity: y / document.body.scrollHeight});
+	useEffect(() => {
+		controls.start({ opacity: y / document.body.scrollHeight });
 	}, [y, controls]);
 
 	const scrollToTop = () => {
 		window.scrollTo({
-			top:0,
+			top: 0,
 			behavior: 'smooth'
 		});
 	};
 
 	return (
-		<motion.div 
-			className={styles.up} 			
+		<motion.div
+			className={styles.up}
 			animate={controls}
-			initial={{opacity:0}}
-		> 
-			<ButtonIcon appearance='primary' icon='up' onClick={scrollToTop}/>
+			initial={{ opacity: 0 }}
+		>
+			<ButtonIcon appearance='primary' icon='up' aria-label="Наверх" onClick={scrollToTop} />
 		</motion.div>
 	);
 };
